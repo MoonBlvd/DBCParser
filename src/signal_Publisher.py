54ea9f8920be39b8fdb9116b8392992db752a700
MoonBlvd <brianyao@umich.edu>
2017-01-05 17:37:23 -0500

    lcm publish added

--- src/signal_Publisher.py
+++ src/signal_Publisher.py
@@ -1,5 +1,13 @@
 import numpy as np
+import os
 import csv
+from datetime import datetime
+
+import lcm
+from JKUlcm import JKU_t
+
+msg = JKU_t()
+lc = lcm.LCM()
 
 def dataReader(filename):
     i = 0
@@ -14,9 +22,14 @@ def dataReader(filename):
                data =  np.vstack((data,line))
             i = i+1
     #print data[:,0]
-    return data
+    return data, i+1
 if __name__ == '__main__':       
     filename = 'exp1_077.csv'
     data = dataReader(filename)
-
-
+    num_rows, num_cols = data.shape
+    msg.num_data = num_cols
+    print num_rows, num_cols
+    for i in range (0,num_rows):
+        msg.utime = datetime.now().time()
+        msg.data =  data[i,:]
+        lc.publish("JKU_data", msg.encode())
