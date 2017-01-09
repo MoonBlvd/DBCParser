002a06ddf3aa03cf14bb2c0bf274b9d0babfab96
MoonBlvd <brianyao@umich.edu>
2017-01-09 15:04:35 -0500

    adding compression thread

--- src/signal_Receiver.py
+++ src/signal_Receiver.py
@@ -2,17 +2,24 @@ import lcm
 import threading
 import numpy as np
 from JKUlcm import JKU_t
-#from msvcrt import getch
+import pygame, sys
+#import pygame.locals()
+import time
 
+pygame.init()
 lc = lcm.LCM()
-#buf_size = 10
+buf_size = 10
 num_data = 805 
+global buf
 buf = np.zeros(num_data)
+global data
+data = np.zeros((buf_size, 805))
 
 def data_handler(channel, data):
     msg = JKU_t.decode(data)
-    #print "Data received time is: ", msg.utime
-    #print "Received message 1 is: ", msg.data[0]
+    print "Data received time is: ", msg.utime
+    print "Received message 1 is: ", msg.data[0]
+    global buf
     buf = msg.data
 
 class lcm_thread(threading.Thread):
@@ -26,37 +33,79 @@ class lcm_thread(threading.Thread):
             while True:
                lc.handle()
         except KeyboardInterrup:
-            pass
+            print "killing lcm-thread..."
+            sys.exit()
+            #pass
         lc.unsubscribe(subsription)
-        
+
 class processing_thread(threading.Thread):
     def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
         self.threadID = threadID
         self.name = name
     def run(self):
         init_data = buf
         prev_buf = buf
+        global buf
         while True:
-            data = np.zeros(buf_size, 805)
+            tmpData = np.zeros((buf_size, 805))
             i = 0
+            #print"Time of buf in processing loop: ", buf[0]
             while i<10:
                 if buf[0] == prev_buf[0]:
                     continue
-                data[i,:] = buf
+                #print"Time of buf in processing loop: ", buf[0]
+                tmpData[i,:] = buf
                 prev_buf = buf
+                i = i + 1
+            global data
+            data = tmpData
+            # Compress the  data batch
+            
+
+class compression_thread(threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        global data
+        i = 0
+        while True:
+            if data[1,0] != 0:
                 i = i+1
-            print data
+                print data
+                if i == 3:
+                    sys.exit()
+
 if __name__ == "__main__":
     lcm_loop = lcm_thread(1, "lcm_thread")
-  #  processing_loop = processing_thread(2, "processing_thread")
-
+    processing_loop = processing_thread(2, "processing_thread")
+    compression = compression_thread(3,"compression_thread")
+    
     lcm_loop.start()
-    prev_buf = buf
-  #  processing_loop.start()
-    while True:
-        key = ord(getch())
-        if key == 27: #ESC
-            break
-        if buf[0] != prev_buf[0]:
-            print buf
+    processing_loop.start()
+    compression.start()
+'''
+    try:
+        while True:
+            print buf[0]
+            if buf[0] != prev_buf[0]:
+                print "in if"
+                print buf
+    except KeyboardInterrup:
+        print "killing lcm-thread..."
+        raise
+        #sys.exit()
 
+    while True:
+        event = pygame.event.wait()
+        #for event in events:
+           # print "waiting for quit..."
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                print "Quit now..."
+                pygame.quit()
+                sys.exit()
+
+'''
