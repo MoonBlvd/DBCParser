5bf9805e9b2395ec94a3b5a35a37e42a10653a8c
brianyao <brianyao@umich.edu>
2017-01-08 20:34:45 -0500

    add lcm_thread and process_thread

--- src/signal_Receiver.py
+++ src/signal_Receiver.py
@@ -1,4 +1,5 @@
 import lcm
+import threading
 from JKUlcm import JKU_t
 
 lc = lcm.LCM()
@@ -7,14 +8,27 @@ def data_handler(channel, data):
     msg = JKU_t.decode(data)
     print "Data received time is: ", msg.utime
     print "Received message 1 is: ", msg.data[0]
+    return msg.data
 
-subscription = lc.subscribe("JKU_data", data_handler)
+class lcm_thread(threading.Thread):
+    def __init__(self,threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        subscription = lc.subscribe("JKU_data", data_handler)
+        try:
+            while True:
+               lc.handle()
+        except KeyboardInterrup:
+            pass
+        lc.unsubscribe(subsription)
+#class processing_thread(threading.Thread):
+#    def __init__(self)
 
-try:
-    while True:
-        lc.handle()
-except KeyboardInterrup:
-    pass
+if __name__ == "__main__":
+    lcm_loop = lcm_thread(1, "lcm_thread")
+#    processing_loop = processing_thread
 
-lc.unsubscribe(subsription)
-
+    lcm_loop.start()
+#    processing_loop.start()
