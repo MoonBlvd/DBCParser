4b9d56772bfe684127cf9f22babdcf684b772543
brianyao <brianyao@umich.edu>
2017-01-13 21:39:46 -0500

    compression problem come from data type, convert string to bitarray

--- src/test.py
+++ src/test.py
@@ -1,9 +1,12 @@
 from compress import lz77_compressor
 import sys
+from bitarray import bitarray
 
 compressor = lz77_compressor(20)
 
-a = 'How are you?'
+a = 'hello world!'
+b = bitarray(a)
 c = compressor.compress(a)
 print c
 print len(c)
+print b[1]
